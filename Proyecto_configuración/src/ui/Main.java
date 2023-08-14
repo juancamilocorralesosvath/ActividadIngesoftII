20af2193bc243be95bb331adf163760a0aa92ade
Juan Camilo Corrales Osvath <1007489003@u.icesi.edu.co>
2023-08-14 11:27:15 -0500

    hola mundo en frances en la rama de juan camilo

--- Proyecto_configuración/src/ui/Main.java
+++ Proyecto_configuración/src/ui/Main.java
@@ -2,6 +2,7 @@ package ui;
 
 public class Main {
 	public static void main(String[] arg) {
-		
+		// cambio de Juan Camilo. Hola mundo en frances
+		System.out.println("Bonjour tout monde!");
 	}
 }
